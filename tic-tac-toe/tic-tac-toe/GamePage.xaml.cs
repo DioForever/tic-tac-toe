5599a74369a44084d94f3e0543e9222546b63b41
Daniel Vaško <vaskodaniel1@gmail.com>
2024-04-20 15:11:05 +0200

    Changed Minimax score calculation

--- tic-tac-toe/tic-tac-toe/GamePage.xaml.cs
+++ tic-tac-toe/tic-tac-toe/GamePage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using tic_tac_toe.Resources;
 
 namespace tic_tac_toe
 {
@@ -22,14 +25,17 @@ namespace tic_tac_toe
     {
         public static GameSettings GameSettings { get; set; }
         public GameSettings gameSettings { get; set; }
-        public bool plaerTurn = true;
+        public bool playerTurn = true;
 
+        private Border[,] borders;
+        private Game game;
+        private byte player = 1;
         public GamePage()
         {
             gameSettings = GameSettings;
 
             int size = gameSettings.size;
-            bool enemyType = gameSettings.enemyType;
+            game = new Game(size);
 
             InitializeComponent();
 
@@ -49,16 +55,19 @@ namespace tic_tac_toe
                 gameGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
             }
 
-            // Create and add TextBoxes to the grid
-            for (int i = 0; i < size; i++)
+            borders = new Border[size, size];
+            
+            for (int y = 0; y < size; y++)
             {
-                for (int j = 0; j < size; j++)
+                for (int x = 0; x < size; x++)
                 {
+
                     // Spawn border for each cell in the grid
                     Border border = new Border();
+                    borders[y, x] = border;
 
-                    border.SetValue(Grid.RowProperty, i);
-                    border.SetValue(Grid.ColumnProperty, j);
+                    border.SetValue(Grid.RowProperty, y);
+                    border.SetValue(Grid.ColumnProperty, x);
                     border.CornerRadius = new CornerRadius(5);
 
 
@@ -69,36 +78,11 @@ namespace tic_tac_toe
 
                     border.Margin = new Thickness(5);
 
-                    // Add on click event to the border so it changes color when clicked
                     border.MouseLeftButtonDown += (sender, e) =>
                     {
-                        if (plaerTurn)
-                        {
-                            /*
-                            BitmapImage btm = new BitmapImage(new Uri("pack://application:,,,/Resources/CROSS.png", UriKind.Relative));
-                            Image cross = new Image();
-
-                            cross.Source = btm;*/
-                            Image Img = new Image();
-                            Img.Source = new BitmapImage(new Uri(
-                                         "pack://application:,,,/tic-tac-toe;component/Resources/CROSS.png"));
-
-                            plaerTurn = false;
-
-                            border.Child = Img;
-
-                        }
-                        else
-                        {
-                            // put image CIRCLE in the border as child
-                            Image Img = new Image();
-                            Img.Source = new BitmapImage(new Uri(
-                                         "pack://application:,,,/tic-tac-toe;component/Resources/CIRCLE.png"));
-
-                            plaerTurn = true;
-
-                            border.Child = Img;
-                        }
+                        int clickedRowY = Grid.GetRow(border);
+                        int clickedColumnX = Grid.GetColumn(border);
+                        player = Place(clickedRowY, clickedColumnX, player, border);
                     };
 
 
@@ -106,6 +90,102 @@ namespace tic_tac_toe
                     gameGrid.Children.Add(border);
                 }
             }
+        }
+
+        public byte Place(int y, int x, byte player, Border border)
+        {
+            byte[,] map = game.board;
+            bool taken = (map[y, x] != 0);
+
+            int winner = 0;
+            bool boardFull = false;
+
+            if(taken) return player;
+
+            if (playerTurn && !taken)
+            {
+
+                spawnImageUnderBorder("Resources/CROSS.png", border);
+                game.board[y, x] = player;
+                byte[,] boardS = game.board;
+                player = 2;
+
+                playerTurn = false;
+
+            }
+            else if (!gameSettings.enemyType && !taken)
+            {
+
+                spawnImageUnderBorder("Resources/CIRCLE.png", border);
+                game.board[y, x] = player;
+
+                player = 1;
+                playerTurn = true;
+            }
+
+            winner = game.GameWon();
+            boardFull = game.BoardFull();
+            AnnounceWinner(winner, boardFull);
+
+            // AI move if enabled
+            if (GameSettings.enemyType)
+            {
+                MoveAI();
+                // use AI to make a move
+                player = 1;
+                playerTurn = true;
+
+            }
+
+            winner = game.GameWon();
+            boardFull = game.BoardFull();
+            AnnounceWinner(winner, boardFull);
+
+
+            byte[,] b = game.board;
+            return player;
+
+        }
+
+        private void MoveAI()
+        {
+            (int, int) move = game.FindBestMove(game);
+            game.MakeMove(move.Item1, move.Item2, 2);
+            spawnImageUnderBorder("Resources/CIRCLE.png", borders[move.Item1, move.Item2]);
+        }
+
+        private void AnnounceWinner(int winner, bool draw)
+        {
+            Game g = game;
+            if(winner != 0)
+            {
+                MessageBox.Show("Player " + winner + " won!");
+                game.InitializeBoard();
+                GenerateGridOfLists(borders.GetLength(0));
+            }
+            else if(draw)
+            {
+                MessageBox.Show("Draw!");
+                game.InitializeBoard();
+                GenerateGridOfLists(borders.GetLength(0));
+            }
+
+        }
+
+        public bool CheckTaken(int y, int x)
+        {
+            return game.board[y, x] != 0;
+        }
+
+        public static void spawnImageUnderBorder(string path, Border border)
+        {
+            // put image CIRCLE in the border as child
+            Image Img = new Image();
+            Img.Source = new BitmapImage(new Uri(
+                                        "pack://application:,,,/tic-tac-toe;component/"+path));
+
+            border.Child = Img;
+            
         }
 
     }
--- tic-tac-toe/tic-tac-toe/Resources/Game.cs
+++ tic-tac-toe/tic-tac-toe/Resources/Game.cs
@@ -1,12 +1,237 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace tic_tac_toe.Resources
 {
     internal class Game
     {
+        // The board is represented as a 2D array
+        private const int winLength = 3;
+        public byte[,] board;
+        private int boardSize;
+
+        public Game(int size)
+        {
+            boardSize = size;
+            board = new byte[size, size];
+            InitializeBoard();
+        }
+
+        public void InitializeBoard()
+        {
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    board[i, j] = 0;
+                }
+            }
+        }
+
+
+
+        public void MakeMove(int row, int col, int player)
+        {
+            board[row, col] = (byte)player;
+        }
+
+        public bool BoardFull()
+        {
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    if (board[i, j] == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public int GameWon()
+        {
+            // Check rows for a win
+            for (int y = 0; y < boardSize; y++)
+            {
+                if (board[y, 0] != 0 &&
+                    board[y, 0] == board[y, 1] &&
+                    board[y, 1] == board[y, 2])
+                {
+                    return board[y, 0]; // Return the winning player
+                }
+            }
+
+            // Check columns for a win
+            for (int x = 0; x < boardSize; x++)
+            {
+                if (board[0, x] != 0 &&
+                    board[0, x] == board[1, x] &&
+                    board[1, x] == board[2, x])
+                {
+                    return board[0, x]; // Return the winning player
+                }
+            }
+
+            // Check diagonals for a win
+            if (board[0, 0] != 0 &&
+                board[0, 0] == board[1, 1] &&
+                board[1, 1] == board[2, 2])
+            {
+                return board[0, 0]; // Return the winning player
+            }
+
+            if (board[0, 2] != 0 &&
+                board[0, 2] == board[1, 1] &&
+                board[1, 1] == board[2, 0])
+            {
+                return board[0, 2]; // Return the winning player
+            }
+
+            return 0; // No player has won yet
+        }
+
+
+        private bool CheckRow(int row, int column, int player)
+        {
+            int count = 0;
+            for (int i = column; i < boardSize; i++)
+            {
+                if (board[row, i] == player)
+                {
+                    count++;
+                }
+            }
+            if (count == winLength) return true;
+            return false;
+        }
+
+        private bool CheckColumn(int row, int column, int player)
+        {
+            int count = 0;
+            for (int i = row; i < boardSize; i++)
+            {
+                if (board[i, column] == player)
+                {
+                    count++;
+                }
+            }
+            if (count == winLength) return true;
+            return false;
+        }
+
+        private bool CheckDiagonal(int row, int column, int player)
+        {
+            int count = 0;
+            for (int i = 0; i < boardSize; i++)
+            {
+                if (board[i, i] == player)
+                {
+                    count++;
+                }
+            }
+            if (count == winLength) return true;
+            return false;
+        }
+
+        public bool CheckWin(int row, int column, int player)
+        {
+            if (CheckRow(row, column, player) || CheckColumn(row, column, player) || CheckDiagonal(row, column, player))
+            {
+                return true;
+            } 
+            return false;
+        }
+
+        public (int, int) FindBestMove(Game game)
+        {
+            int bestScore = int.MinValue;
+            (int, int) bestMove = (0,0);
+            byte[,] bytes = game.board;
+
+            for (int y = 0; y < boardSize; y++)
+            {
+                for (int x = 0; x < boardSize; x++)
+                {
+                    bool open = game.board[y, x] == 0;
+                    if (game.board[y, x] == 0)
+                    {
+                        game.board[y, x] = 2; // Assuming AI is player 2
+                        int score = Minimax(game, 1, 2, true);
+                        game.board[y, x] = 0;
+
+                        if (score > bestScore)
+                        {
+                            bestScore = score;
+                            bestMove = (y, x);
+                        }
+                    }
+                }
+            }
+
+            return bestMove;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="game"></param>
+        /// <param name="depth"></param>
+        /// <param name="player"></param>
+        /// <param name="mode">True if Maximizing, False if Minimazing</param>
+        /// <returns></returns>
+        static int Minimax(Game game, int depth, byte player, bool isMaximizing)
+        {
+            if(game.GameWon() == 2)
+            {
+                return 10 + depth;
+            }
+            else if(game.GameWon() == 1)
+            {
+                return 10 - depth;
+            }
+            else if(game.BoardFull())
+            {
+                return 10 - depth;
+            }
+
+            if (isMaximizing)
+            {
+                int bestScore = int.MinValue;
+                for (int i = 0; i < game.boardSize; i++)
+                {
+                    for (int j = 0; j < game.boardSize; j++)
+                    {
+                        if (game.board[i, j] == 0)
+                        {
+                            game.board[i, j] = player;
+                            int score = Minimax(game, depth + 1, player, false);
+                            game.board[i, j] = 0;
+                            bestScore = Math.Max(score, bestScore);
+                        }
+                    }
+                }
+                return bestScore;
+            }
+            else
+            {
+                int bestScore = int.MaxValue;
+                for (int i = 0; i < game.boardSize; i++)
+                {
+                    for (int j = 0; j < game.boardSize; j++)
+                    {
+                        if (game.board[i, j] == 0)
+                        {
+                            game.board[i, j] = player;
+                            int score = Minimax(game, depth + 1, player, true);
+                            game.board[i, j] = 0;
+                            bestScore = Math.Min(score, bestScore);
+                        }
+                    }
+                }
+                return bestScore;   
+            }
+
+        }
     }
 }
